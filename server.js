c7e8ac97b5359557f52771c9ea177152c4553f51
キノシタ <patthadon2545@gmail.com>
2025-04-21 17:06:45 +0700

    wrong port must be 80 instead of 3000

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const { fetch } = require("undici");
 dotenv.config();
 const app = express();
 app.use(cors());
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Ensure form data is parsed
